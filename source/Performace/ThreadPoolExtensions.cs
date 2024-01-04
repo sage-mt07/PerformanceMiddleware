42fd05dee262ca6855e02b2ce580c2aa9182c2eb
seiji@tet.jp <seiji@ttt.jp>
2024-01-04 18:44:56 +0900

    update

---
+++ source/Performace/ThreadPoolExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Performace
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IHostApplicationBuilder ConfigureThreadPool(this IHostApplicationBuilder hostBuilder)
+        {
+            var settings = hostBuilder.Configuration.Get<ThreadPoolSettings>();
+            ThreadPool.GetMinThreads(out int workerThreads, out int completionPortThreads);
+            if (workerThreads < settings.MinWorkerThreads)
+                workerThreads = settings.MinWorkerThreads;
+            if (completionPortThreads < settings.MinCompletionThreads)
+                completionPortThreads = settings.MinCompletionThreads;
+
+
+            if (settings != null)
+            {
+                ThreadPool.SetMinThreads(settings.MinWorkerThreads, settings.MinCompletionThreads);
+                _logger.LogInformation($"workerThreads={workerThreads} completionPortThreads={completionPortThreads}");
+            }
+            var monitor = new ThreadPoolMonitor();
+            monitor.ThreadPoolSizeIncreased += (current, previos) =>
+            {
+               // _logger.LogWarning("");
+            };
+            return hostBuilder;
+        }
+    }
+}
---
+++ source/Performace/ThreadPoolSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Performace
+{
+    public class ThreadPoolSettings
+    {
+        public int MinWorkerThreads { get; set; }
+        public int MinCompletionThreads { get; set; }
+    }
+}
